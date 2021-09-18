202d1c722c8e0b6bda21a991ebbf54eadf3f7436
Allan Zhou <allanlinzhou@gmail.com>
2021-09-18 10:24:42 -0400

    Completed Resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -8,10 +8,35 @@ class Resume extends React.Component {
             <h2 className="resume-title">Resume</h2>
 
             <div className="experience-list">
-                <b className="resume-heading">Job Experience</b> <br></br>
-                    
-                <p className="list-point">A&W Team Member <br></br>June 2021 - September 2021</p> 
-                    
+              <b className="resume-heading work">Job Experience</b> <br></br>
+              <p className="list-point">
+                <b className="title">A&W Team Member</b> <br></br>June 2021 -
+                September 2021
+              </p>
+              <b className="resume-heading leadership">Leadership Experience</b>{" "}
+              <br></br>
+              <p className="list-point">
+                <b className="title">Robotics Programming Lead, Arctos 6135</b>
+                <br></br>May 2021 - Present
+              </p>
+              <p className="list-point">
+                <b className="title">
+                  President of Bloor C.I. Python A.I. Club
+                </b>
+                <br></br>August 2021 - Present
+              </p>
+              <b className="resume-heading volunteer">Volunteer Experience</b>{" "}
+              <br></br>
+              <p className="list-point">
+                <b className="title">
+                  Robotics Camp Ursa Minor, Coding Counsellor
+                </b>
+                <br></br>May 2021 - August 2021
+              </p>
+              <p className="list-point">
+                <b className="title">Camp GTA Volunteer</b>
+                <br></br>July 2019 - August 2019
+              </p>
             </div>
           </div>
         );
