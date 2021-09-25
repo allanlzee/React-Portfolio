7296d1695d40e228288cc198a06a31b462e2b688
Allan Zhou <allanlinzhou@gmail.com>
2021-09-25 11:21:55 -0400

    Updated Violin Section

--- src/components/violin/Violin.jsx
+++ src/components/violin/Violin.jsx
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
-import "./Violin.css"; 
+import "./Violin.css";
+import Concerto from "./Images/Concerto.png";
+import Allegro from "./Images/Allegro.png";
+import Chamber1 from "./Images/Chamber.png";
+import Chamber2 from "./Images/Chamber2.png"; 
 
 class Violin extends React.Component {
     render() { 
-        return <section className="violin-section">
+        return (
+          <section className="violin">
             <h2 className="violin-section-title">Violin Performances</h2>
 
-            <h3 className="violin-section-subtitle">Solo Performances</h3>
+            <h3 className="violin-section-subtitle">Solo</h3>
 
-            <div className="solo-performances"> 
+            <div className="violin-section"> 
+                <div className="performances-concerto solo-performances">
+                  <p className="piece">
+                    <span className="piece-name">Concerto in G Major</span> <br></br>W.A. Mozart
+                  </p>
 
-                <div className="performances">
-                    <p className="piece-name">Concerto in G Major <br></br>W.A. Mozart</p>
+                  <a
+                    className="violin-video"
+                    href="https://www.youtube.com/embed/1JLYBkrh8iM?start=4024"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="thumbnail"
+                      alt="Concerto in G Major -- W.A. Mozart"
+                      src={Concerto}
+                    />
+                  </a>
+                </div>
 
+                <div className="performances-allegro solo-performances">
+                  <p className="piece">
+                    <span className="piece-name">Allegro Brillante </span><br></br>W.T. Have
+                  </p>
+
+                  <a
+                    className="violin-video"
+                    href="https://www.youtube.com/watch?v=smqnNrgdz7E&ab_channel=UniversitySettlementMusic%26ArtsSchool"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="thumbnail"
+                      alt="Concerto in G Major -- W.A. Mozart"
+                      src={Allegro}
+                    />
+                  </a>
+                </div>
                     
-                </div> 
-
             </div>
-
-        </section>;
+          </section>
+        ); 
     }
 }
  
